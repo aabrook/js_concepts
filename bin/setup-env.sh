c1ab01e88e078af90c746001006ea0032703cbe5
Aaron Abrook <aabrook@gmail.com>
2017-05-28 13:08:56 +1000

    Add development setup

--- bin/setup-env.sh
+++ bin/setup-env.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-mkdir -p ./events/db/store/test
-touch ./events/db/store/test/events.db
 cd events
+
+mkdir -p ./db/store/test
+touch ./db/store/test/events.db
 NODE_ENV=test ../node_modules/.bin/sequelize db:migrate
 
+mkdir -p ./db/store/development
+touch ./db/store/development/events.db
+NODE_ENV=development ../node_modules/.bin/sequelize db:migrate
