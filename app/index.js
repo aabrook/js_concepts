a4a3220c4815b591a4c1771d717f3304542ad30a
Aaron Abrook <aabrook@gmail.com>
2017-05-28 21:48:46 +1000

    Cors and tidy up

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const parser = require('body-parser')
+const cors = require('cors')
 const { createStore } = require('../store')
 const Sequelize = require('sequelize')
 const sequelize = new Sequelize('events', null, null, {
@@ -12,12 +13,10 @@ const { log } = require('../helpers')
 const addedTask = (state, { type, task }) =>
   type === 'addedTask' ? assign({}, state, { tasks: [...state.tasks, { task }] }) : state
 
-const deletedTask = (state, { type, task }) => {
-  console.log('DEBUG', state.tasks)
-  return type === 'deletedTask'
-    ? assign({}, state, { tasks: state.tasks.filter(({ id }) => id !== parseInt(task.id)) })
+const deletedTask = (state, { type, task }) =>
+  type === 'deletedTask'
+    ? assign({}, state, { tasks: state.tasks.filter((t) => t.task.id !== parseInt(task.id)) })
     : state
-}
 
 const myStore = createStore({tasks: []}, [addedTask, deletedTask])
 const taskState = log((...args) => console.log('state', ...args))((event) =>
@@ -30,6 +29,7 @@ const config = {
 }
 
 const app = express()
+app.use(cors())
 app.use(parser.json())
 
 app.use('/tasks', require('./routes')({ store: eventStore, state: () => myStore.state, projections: { taskState } }))
