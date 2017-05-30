8ec3add8c6f1e79f3bcb1114ab49ae0c729a4857
Aaron Abrook <aabrook@gmail.com>
2017-05-30 21:25:46 +1000

    Add linting

--- app/routes/events.js
+++ app/routes/events.js
@@ -3,11 +3,11 @@ const { parse } = JSON
 
 module.exports = ({ projections, state, store: { allEvents, storeEvent, models: { Event } } }) => {
   const route = express()
-  route.get('/', (req, res) => (
+  route.get('/', (_req, res) => (
     allEvents()
       .then(events => events.map(ev => ev.dataValues))
       .then(events => res.send(events))
-      .catch(res.send)
+      .catch(e => res.send(e))
   ))
 
   return route
