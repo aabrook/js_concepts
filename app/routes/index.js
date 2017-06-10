3f2aaff171e14a7ab91cd98b5b4cb7c9ea4ca526
Aaron Abrook <aabrook@gmail.com>
2017-06-10 13:29:07 +1000

    Rely on the ID passed between events for responding

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,40 +2,56 @@ const express = require('express')
 const uuid = require('uuid/v4')
 const { assign } = Object
 const { stringify } = JSON
+const { Try } = require('../../helpers')
 
-module.exports = ({ state, store: { emit, storeEvent } }) => {
+module.exports = ({ state, store: { emit, on, remove, storeEvent } }) => {
   const route = express()
 
   route.post('/', (req, res) => {
     const id = uuid()
+    const sender = (payload, reqId) =>
+      (reqId === id ? (
+        Try(
+          (payload) => res.send(payload)
+        )(
+          (resp) => console.log('send', resp),
+          console.error
+        )(payload) && remove(sender)
+      ) : payload)
 
+    on('stateUpdated', sender)
     return storeEvent({
       aggregate: 'tasks',
       event: assign({ type: 'addedTask' }, req.body)
     }).then(
       ({ dataValues: { event } }) => emit('addedTask', event, id)
-    ).then(
-      results => console.log('ADDED', results) || res.send(results[0])
     ).catch(
       e => console.error('Failed to add and emit', e) || res.send(stringify({ error: e }))
     )
   })
 
   route.get('/', (req, res) => (
-    console.log(state()) ||
     res.send(state())
   ))
 
   route.delete('/:id', (req, res) => {
     const id = uuid()
+    const sender = (payload, reqId) =>
+      (reqId === id ? (
+        Try(
+          (payload) => res.type('application/json').send(payload)
+        )(
+          (resp) => console.log('send', resp),
+          console.error
+        )(payload) && remove(sender)
+      ) : payload)
 
+    on('stateUpdated', sender)
     return storeEvent({
       aggregate: 'tasks',
       event: { type: 'deletedTask', id: req.params['id'] }
     }).then(
       ({ dataValues: { event } }) => emit('deletedTask', event, id)
-    ).then(projections =>
-      res.type('application/json').send(projections[0])
     ).catch(e =>
       console.error('Failed to delete and emit', e) || res.send(stringify(e))
     )
