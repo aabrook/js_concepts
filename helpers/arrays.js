070ffc4b13564097521c3fa11638b677e40b88b3
Aaron Abrook <aabrook@gmail.com>
2017-06-13 21:56:39 +1000

    Curry the array helpers

--- helpers/arrays.js
+++ helpers/arrays.js
@@ -1,10 +1,11 @@
+const { curry } = require('./')
 
 const zip = (a, b) => a.map((v, i) => [v, b[i]])
-const zipWith = (f) => (a, b) => a.map((v, i) => f(v, b[i]))
-const cycle = (count) => ([h, ...t]) => count > 0 ? [...(cycle(count - 1)([...t, h])), h] : []
+const zipWith = (f, a, b) => a.map((v, i) => f(v, b[i]))
+const cycle = (count, [h, ...t]) => count > 0 ? [...(cycle(count - 1, [...t, h])), h] : []
 
 module.exports = {
-  cycle,
-  zip,
-  zipWith
+  cycle: curry(cycle),
+  zip: curry(zip),
+  zipWith: curry(zipWith)
 }
