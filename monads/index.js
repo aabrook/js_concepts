e298b1b68a6213b0e4f8bca31a573b2de4dcda5e
Aaron Abrook <aabrook@gmail.com>
2017-07-15 22:25:38 +1000

    Try out DrBoolean's do notation generator
    https://github.com/DrBoolean/freeky/blob/master/monad.js

--- monads/index.js
+++ monads/index.js
@@ -2,3 +2,18 @@ exports.Maybe = require('./maybe_monad')
 exports.Reader = require('./reader_monad')
 exports.Writer = require('./writer_monad')
 exports.State = require('./state_monad')
+
+const mdo = (gen) => {
+  const g = gen()
+  const step = value => {
+    const result = g.next(value)
+    return result.done
+      ? result.value
+      : result.value.chain(step)
+  }
+  return step()
+}
+
+exports.Monad = {
+  do: mdo
+}
